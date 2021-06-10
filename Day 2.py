62f4aa0e132926dc9cde899cd7800f2be43f53ee
mmondal <mmondal@codeuridea.com>
2021-06-11 00:01:49 +0530

    Day 2 assignment

---
+++ Day 2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+name = 'Mrinmoy Mondal'
+age = 29
+occupation = 'Python Developer'
+salary = 12250.65
+
+name_st = "My name is {}.".format(name)
+print(name_st)
+
+age_st = "I am {} years old.".format(age)
+print(age_st)
+
+occupation_st = "I work as an {}.".format(occupation)
+print(occupation_st)
+
+salary_st = "I earn Rs.{}/- per month.".format(salary)
+print(salary_st)
+
